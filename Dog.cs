cac30d84f4b7531336b776ee49baeb9f075a0fe1
User <hstockwell151@gmail.com>
2022-12-01 11:51:22 +1000

    Updated dog class

--- Dog.cs
+++ Dog.cs
@@ -1,1 +1,10 @@
-/* Dog.cs */ 
+/*
+	File name: Dog.cs
+	Purpose:	Class to describe pet dog
+			used for a vet practice
+	Author:	Hamish Stockwell
+	Date:		01-12-2022
+	Version:	1.0
+	Notes:	...
+*/
+
